a07f63bcccbea5417aef2c5c9316cc5eb73120b0
CodeBeans2022 <114496238+CodeBeans2022@users.noreply.github.com>
2022-11-25 16:30:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 let sum = "";
+let decimalCheck = []
 
 function addToSum(operator) {
-  sum = sum + operator;
-  document.getElementById("output").innerText = sum;
+    switch(operator){
+        case `,` :
+        if(decimalCheck.includes(`,`)){
+            document.getElementById('output').innerText = document.getElementById('output').innerText
+        } 
+        else{
+            sum += operator
+            document.getElementById('output').innerText = sum
+            decimalCheck.push(operator)
+        }
+        break;
+        case `+`:
+        case `-`:
+        case `*`:
+        case `/`:
+            sum = sum + operator;
+            document.getElementById("output").innerText = sum;
+            decimalCheck = []
+        break;
+        default:
+            sum = sum + operator;
+            document.getElementById("output").innerText = sum;
+            decimalCheck.push(operator)
+            operatorCheck.push(operator)
+    }
 }
 
 function evaluateSum() {
@@ -12,9 +36,12 @@ function evaluateSum() {
 function clearSum() {
   sum = "";
   document.getElementById("output").innerText = sum;
+  decimalCheck = []
 }
 
 function backspace() {
   sum = sum.slice(0, -1);
   document.getElementById("output").innerText = sum;
+  decimalCheck.pop()
 }
+
